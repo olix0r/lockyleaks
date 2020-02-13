c17a8feb1d12360994e6625a720adbbd32c3aa70
Oliver Gould <ver@buoyant.io>
2020-02-13 00:58:30 +0000

    Change the test to exit gracefully

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,58 @@
-use futures::{Async, future, try_ready, Future, Poll};
+use futures::{future, try_ready, Async, Future, Poll, Stream};
 use std::time::Duration;
-use tokio::sync::lock;
+use tokio::sync::{lock, mpsc};
 
 fn main() {
     tokio::run(future::lazy(|| {
         let lock = lock::Lock::new(());
 
-        for _ in 0..10_000 {
-            let fut = Loop {
+        let concurrency = 10000;
+        let (tx, rx) = mpsc::channel(1);
+        for _ in 0..concurrency {
+            tokio::spawn(Loop {
                 lock: lock.clone(),
                 pending: None,
-            };
-            tokio::spawn(fut);
+                remaining: 109,
+                _tx: tx.clone(),
+            });
         }
 
-        future::empty()
+        rx.fold((), |_, _| Ok(())).map_err(|_| panic!())
     }));
 }
 
 struct Loop {
     lock: lock::Lock<()>,
     pending: Option<tokio::timer::Timeout<AcquireAndIdle>>,
+    remaining: usize,
+    _tx: mpsc::Sender<()>,
+}
+
+impl Future for Loop {
+    type Item = ();
+    type Error = ();
+
+    fn poll(&mut self) -> Poll<(), ()> {
+        loop {
+            if let Some(pending) = self.pending.as_mut() {
+                if let Ok(Async::NotReady) = pending.poll() {
+                    return Ok(Async::NotReady);
+                }
+                self.remaining -= 1;
+                self.pending = None;
+            }
+
+            if self.remaining == 0 {
+                return Ok(Async::Ready(()));
+            }
+
+            let fut = AcquireAndIdle {
+                lock: self.lock.clone(),
+                locked: None,
+            };
+            self.pending = Some(tokio::timer::Timeout::new(fut, Duration::from_millis(100)));
+        }
+    }
 }
 
 struct AcquireAndIdle {
@@ -40,28 +72,6 @@ impl Future for AcquireAndIdle {
 
             let guard = try_ready!(Ok(self.lock.poll_lock()));
             self.locked = Some(guard);
-        }
-    }
-}
-
-impl Future for Loop {
-    type Item = ();
-    type Error = ();
-
-    fn poll(&mut self) -> Poll<(), ()> {
-        loop {
-            if let Some(pending) = self.pending.as_mut() {
-                if let Ok(Async::NotReady) = pending.poll() {
-                    return Ok(Async::NotReady);
-                }
-                self.pending = None;
-            }
-
-            let fut = AcquireAndIdle {
-                lock: self.lock.clone(),
-                locked: None,
-            };
-            self.pending = Some(tokio::timer::Timeout::new(fut, Duration::from_millis(1000)));
         }
     }
 }
